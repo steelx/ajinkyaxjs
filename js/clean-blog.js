7356f4bcdb51552c78be6724cea71059b5eef5f6
Ajinkya Borade <ajinkyab1987@gmail.com>
2015-04-21 12:35:05 +0530

    updated js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,21 +1,48 @@
 //Lightbox
-var imglinks = $(".lightbox a"),
-    cover = document.getElementById("cover"),
-    coverimage = cover.getElementsByTagName("img")[0];
-    testdialog = document.createElement("dialog");
-    testdialog.setAttribute("open", "");
-if (!testdialog.open) { dialogPolyfill.registerDialog(cover); }
-for (var i=0; i<imglinks.length; i++) {
-  imglinks[i].onclick = showImage;
-}
+$(function() {
 
-function showImage(e) {
-  e.preventDefault();
-  coverimage.setAttribute("src", this.getAttribute("href"));
-  coverimage.setAttribute("alt", this.querySelector("img").getAttribute("alt"));
-  cover.showModal();
-}
-document.getElementById("closecover").onclick = function() {
-  coverimage.setAttribute("src", "");
-  cover.close();
-}
+  $('.lightbox').on("click", function(e) {
+		e.preventDefault();
+    console.log('You clicked the image');
+
+    var windowHeight = window.innerHeight || $(window).height(),
+      	windowWidth = window.innerWidth || $(window).width();
+
+    // Create the overlay, append it to body and make it visible.
+    $('<div id="overlay"></div>')
+      .css('opacity', '0')
+      .animate({
+        'opacity': '0.5'
+      }, 'slow')
+      .appendTo('body');
+
+    // Create the lightbox container which shall contain the image
+    $('<div id="lightbox"></div>')
+      .hide()
+      .appendTo('body');
+
+    // Create img-element and add to #lightbox when loaded.
+    $('<img>')
+      .attr('src', $(this).attr('href'))
+      .css({
+        'max-height': windowHeight,
+        'max-width': windowWidth
+      })
+      .load(function() {
+        $('#lightbox')
+          .css({
+            'top': (windowHeight - $('#lightbox').height()) / 2,
+            'left': (windowWidth - $('#lightbox').width()) / 2
+          })
+          .fadeIn();
+      })
+      .appendTo('#lightbox');
+
+    // Remove lightbox on click
+    $('#overlay, #lightbox').on("click", function() {
+      $('#overlay, #lightbox').remove();
+    });
+
+  });
+
+});
