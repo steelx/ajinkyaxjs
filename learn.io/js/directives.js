f8c78bab19c0a0ee4ad844c530021e7886e37482
Steel <ajinkya@riva.co>
2015-09-03 20:21:29 +0530

    Directives js

---
+++ learn.io/js/directives.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function(angular){
+angular.module('learnApp', [])
+    .directive('stickyHeader', function(){
+
+        return {
+            restrict: 'A',
+            link: function(scope, elem, attrs){
+                var $header = elem,
+                    $body = angular.element('body'),
+                    $window = angular.element(window);
+
+                $window.bind('scroll' ,function () {
+
+                    if ($window.width() >= 1000) {
+
+                        var offset = Number(attrs.offset);
+
+                        if ($body.scrollTop() >= offset) {
+                            $header.addClass('fixed');
+                        } else if ($body.scrollTop() < offset) {
+                            $header.removeClass('fixed');
+                        }
+                    }
+
+                });
+            }
+        }
+    })
+
+    .directive('includeReplace', function () {
+        return {
+            require: 'ngInclude',
+            restrict: 'A',
+            link: function (scope, el, attrs) {
+                el.replaceWith(el.children());
+            }
+        };
+    });
+})(window.angular);
